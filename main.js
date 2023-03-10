8ce4be612909343ae5555d41daf99b141eeb80a7
WaffiqA <waffiq.a2002@gmail.com>
2023-03-10 09:43:17 +0700

    Update output

--- main.js
+++ main.js
@@ -16,21 +16,22 @@ function showData(
   discount = "10",
   publisher = "Elex Media Komputindo"
 ) {
-  const tax = 10;
+  const tax = 5;
   var tempPrice = 0
   tempPrice += countDiscount(price, discount); // addition assignment
 
+  console.clear()
   console.log(`Judul\t\t\t: ${title}`);
   console.log(`Penulis\t\t\t: ${writer}`);
   console.log(`Edisi\t\t\t: ${edition}`);
   console.log(`Harga\t\t\t: ${price}`);
   console.log(`ISBN\t\t\t: ${isbn}`);
   console.log(`Penerbit\t\t: ${publisher}\n`);
-  console.log(`Diskon\t\t\t: ${discount}%`);
-  console.log(`Harga Diskon\t\t: ${tempPrice}\n`);
+  console.log(`Diskon\t\t\t: ${discount}% (-${price * discount / 100})`);
+  console.log(`Harga Setelah Diskon\t: ${tempPrice}\n`);
   console.log(`Harga Sebelum PPN\t: ${tempPrice}`);
-  console.log(`PPN\t\t\t: ${tax}%`);
-  console.log(`Total Harga\t\t: ${countTax(tempPrice, tax)}`);
+  console.log(`PPN\t\t\t: ${tax}% (+${tempPrice * tax / 100})`);
+  console.log(`Harga Akhir\t\t: ${countTax(tempPrice, tax)}`);
 }
 
 showData()
